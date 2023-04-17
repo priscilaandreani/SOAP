61d09da3aef2bfb749dcb2c9b9795a237efb19a3
priscila andreani <priscilaandreani@gmail.com>
2023-04-17 15:30:12 -0300

    fix: add express

--- request.js
+++ request.js
@@ -1,35 +1,65 @@
+const express = require('express');
 const soap = require('soap');
-const HttpProxyAgent = require('http-proxy-agent');
 
-const url = 'http://www.dneonline.com/calculator.asmx?WSDL';
-const args = { intA: 10, intB: 5 };
-const proxy = 'http://localhost:8888';
+const app = express();
 
-const agent = new HttpProxyAgent(proxy);
+const url = 'http://www.dneonline.com/calculator.asmx?wsdl';
 
-const options = {
-  request: {
-    agent: agent
-  },
-  endpoint: url,
-  forceSoap12Headers: true
-}
+app.get('/', function (req, res) {
+  res.send('Bem-vindo ao consumidor de Web Service SOAP com Express!');
+});
 
-soap.createClient(url, options, function(err, client) {
-  if (err) throw err;
+app.get('/add/:intA/:intB', function (req, res) {
+  const args = {
+    intA: parseInt(req.params.intA),
+    intB: parseInt(req.params.intB),
+  };
 
-  client.Add(args, function(err, result) {
+  soap.createClient(url, function (err, client) {
     if (err) throw err;
-    console.log('Add result:', result.AddResult);
 
-    client.Subtract(args, function(err, result) {
+    client.Add(args, function (err, result) {
       if (err) throw err;
-      console.log('Subtract result:', result.SubtractResult);
 
-      client.Multiply(args, function(err, result) {
-        if (err) throw err;
-        console.log('Multiply result:', result.MultiplyResult);
-      });
+      res.send(`Resultado: ${result.AddResult}`);
     });
   });
+});
+
+app.get('/subtract/:intA/:intB', function (req, res) {
+  const args = {
+    intA: parseInt(req.params.intA),
+    intB: parseInt(req.params.intB),
+  };
+
+  soap.createClient(url, function (err, client) {
+    if (err) throw err;
+
+    client.Subtract(args, function (err, result) {
+      if (err) throw err;
+
+      res.send(`Resultado: ${result.SubtractResult}`);
+    });
+  });
+});
+
+app.get('/multiply/:intA/:intB', function (req, res) {
+  const args = {
+    intA: parseInt(req.params.intA),
+    intB: parseInt(req.params.intB),
+  };
+
+  soap.createClient(url, function (err, client) {
+    if (err) throw err;
+
+    client.Multiply(args, function (err, result) {
+      if (err) throw err;
+
+      res.send(`Resultado: ${result.MultiplyResult}`);
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Aplicação rodando na porta 3000!');
 });
